b200705966a33fe66d83e5bacb19ae8761a2f99f
Andrew Lindberg <alindberg@fastmail.net>
2022-12-08 01:07:08 -0500

    solved 8.2

--- 8/eight.py
+++ 8/eight.py
@@ -1,3 +1,4 @@
+import functools
 import os
 
 def print_grid(grid):
@@ -6,18 +7,29 @@ def print_grid(grid):
 
 def path_reveals_tree(trees):
     """list of trees from edge to target"""
-    print(trees, end="")
+    # print(trees, end="")
     height = trees[-1]
     for each in trees[:-1]:
         if each >= height:
-            print(" H")
+            # print(" H")
             return False
-    print(" V")
+    # print(" V")
     return True
-    
 
-def tree_is_visible(tree_row, tree_col, grid):
-    tree_height = grid
+def scenic_distance(trees):
+    print(trees, end="")
+    height = trees[-1]
+    distance = 0
+    for each in trees[:-1][::-1]:
+        if each < height:
+            distance += 1
+        else:
+            distance +=1
+            break
+    return distance
+
+def get_paths(tree_row, tree_col, grid):
+
     top_path = []
     for row in range(0, tree_row+1):
         top_path.append(grid[row][tree_col])
@@ -26,17 +38,26 @@ def tree_is_visible(tree_row, tree_col, grid):
     for row in range(len(grid)-1, tree_row-1, -1):
         bottom_path.append(grid[row][tree_col])
     
+    right_path = []
+    for col in range(len(grid[0])-1, tree_col-1, -1):
+        right_path.append(grid[tree_row][col])
+    
     left_path = []
     for col in range(0, tree_col+1):
         left_path.append(grid[tree_row][col])
     
-    right_path = []
-    for col in range(len(grid[0])-1, tree_col-1, -1):
-        right_path.append(grid[tree_row][col])
+    return [top_path, bottom_path, right_path, left_path]
 
-    if any(map(path_reveals_tree, [top_path, bottom_path, right_path, left_path])):
+
+def tree_is_visible(tree_row, tree_col, grid):
+
+    if any(map(path_reveals_tree,get_paths(tree_row,tree_col, grid))):
         return True
     return False
+
+def scenic_score(tree_col, tree_row, grid):
+    return functools.reduce(lambda x,y: x*y, map(scenic_distance, get_paths(tree_row,tree_col, grid)))
+
 
 lines = []
 with open(os.environ["IN"],"r") as file:
@@ -55,9 +76,21 @@ visible_trees = len(grid) *2 + ( len(grid[0]) -2 )*2
 
 for row in range(1, len(grid)-1):
     for col in range(1, len(grid[0])-1):
-        print(f"checking {row},{col}")
+        # print(f"checking {row},{col}")
         if tree_is_visible(row, col, grid):
             visible_trees += 1
 print(f"Grid: {width} x {height}")
 print(f"Total Trees:   {width*height}")
 print(f"Visible Trees: {visible_trees}")
+
+
+max_score = 0
+for row in range(0, len(grid)):
+    for col in range(0, len(grid[0])):
+        print(f"viewing {row},{col}")
+        max_score = max(max_score, scenic_score(row,col,grid))
+
+print(f"Max Scenic Score: {max_score}")
+
+
+
