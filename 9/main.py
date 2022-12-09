8f710943f0a065625f59c91048c5ec61a45257df
Andrew Lindberg <alindberg@fastmail.net>
2022-12-09 08:44:25 -0500

    solved 9.2

--- 9/main.py
+++ 9/main.py
@@ -8,22 +8,26 @@ with open(os.environ["IN"],"r") as file:
     lines = file.readlines()
 
 
-head = nine.Loc(0,0)
-tail = nine.Loc(0,0)
+knots = {}
+for each in range(10):
+    knots[ each ] = nine.Loc(0,0)
+
 tail_visits = []
-tail_visits.append(tail.get_loc())
+tail_visits.append(knots[9].get_loc())
 for line in lines:
     direction, count = line.strip().split()
     count = int(count)
     
-    movement = {"R": head.right,
-                "L": head.left,
-                "D": head.down,
-                "U": head.up}[direction]
+    movement = {"R": nine.Loc.right,
+                "L": nine.Loc.left,
+                "D": nine.Loc.down,
+                "U": nine.Loc.up}[direction]
     for step in range(count):
-        movement() # move the head
-        tail.move(*nine.tail_follows(*head.get_loc(),*tail.get_loc()))
-        tail_visits.append(tail.get_loc())
+        movement(knots[0]) # move the head
+        for idx in range(1,len(knots)):
+            prev_idx = idx - 1
+            knots[idx].move(*nine.tail_follows(*knots[prev_idx].get_loc(),*knots[idx].get_loc()))
+        tail_visits.append(knots[9].get_loc())
 
 print(f"Visits: {len(set(tail_visits))}")
 
